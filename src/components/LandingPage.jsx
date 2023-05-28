b20b9e9615d381abfc6ec85739998f92a6c4eb66
EllisChang97 <ellischang99@gmail.com>
2023-05-28 18:31:23 -0400

    commit

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -5,7 +5,7 @@ export default function LandingPage(){
             <div id="hero-content">
                 <p id="my_intro__name">Hi, I'm Ellis</p>
 
-                <p id="my_intro__description">I'm a Web Developer focused on delivering clean, efficient, and user-friendly solutions that meet the specific needs of each project.</p>
+                <p id="my_intro__description">I'm a Web Developer focused on delivering clean, efficient, and user-friendly solutions that meet the specific needs of each project...</p>
             </div>
         </div>
         <div id="down-arrow-container">
